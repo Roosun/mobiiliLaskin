22a6fe8c400fc056127adb1d08da7a0686089ce3
Roosun <roosa.sundstrom@hotmail.fi>
2024-08-29 15:34:31 +0300

    added history

--- App.js
+++ App.js
@@ -1,34 +1,44 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 
 export default function App() {
   const [num1, setNum1] = useState('');
   const [num2, setNum2] = useState('');
   const [result, setResult] = useState('');
+  const [history, setHistory] = useState([]);
 
   const handleAdd = () => {
-    setResult(Number(num1) + Number(num2));
+    const additionResult = Number(num1) + Number(num2);
+    setResult(additionResult);
+    setHistory([ { key: `${num1} + ${num2} = ${additionResult}`}, ...history ]);
+    setNum1('');
+    setNum2('');
   };
 
   const handleSubstract = () => {
-    setResult(Number(num1) - Number(num2));
+    const substractionResult = Number(num1) - Number(num2)
+    setResult(substractionResult);
+    setHistory([ {key: `${num1} - ${num2} = ${substractionResult}`}, ...history ]);
+    setNum1('');
+    setNum2('');
   };
 
 
   return (
     <View style={styles.container}>
       <Text style={styles.resultText}>Result: {result}</Text>
-
       <TextInput 
         style={styles.input} 
         keyboardType='numeric'
-        onChangeText={num1 => setNum1(num1)}
+        onChangeText={setNum1}
+        value={num1}
       />
       <TextInput
         style={styles.input} 
         keyboardType='numeric'
-        onChangeText={num2 => setNum2(num2)}
+        onChangeText={setNum2}
+        value={num2}
       />
 
       <View style={styles.buttonContainer}>
@@ -36,13 +46,20 @@ export default function App() {
           <View style={{ width: 20 }} />
         <Button title ="-" onPress={handleSubstract}/>
       </View>
-      
-      
+
+      <Text style={styles.historyText}>History</Text>    
+
+      <View style={styles.listContainer}>
+      <FlatList
+        data={history}
+        renderItem={({ item }) => <Text >{item.key}</Text>}
+        ListEmptyComponent={<Text>No history available</Text>}
+      />  
+      </View>
     </View>
 
   );
 }
-
 
 
 const styles = StyleSheet.create({
@@ -53,23 +70,29 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     paddingTop: '33%',
   },
-
   resultText: {
     fontSize: 20,
   },
-
   input: {
     width: 200,
     height: 40,
     borderColor:'gray',
     borderWidth: 1,
+    textAlign: 'center',
   },
-
   buttonContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 20,
   },
-
+  historyText: {
+    fontSize: 20,
+    marginTop: 30
+  },
+  listContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
